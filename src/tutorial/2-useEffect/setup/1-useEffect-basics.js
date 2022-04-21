14a4ec6c0694a8746e8f4649ad929e653b2bfc18
91639 <goel.aryan2406@gmail.com>
2022-04-21 15:08:10 +0530

    useEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -11,7 +11,7 @@ const UseEffectBasics = () => {
     if (value >= 1) {
       document.title = `new messages(${value})`;
     }
-  }, []);
+  }, [value]);
   console.log("render component");
   return (
     <>
